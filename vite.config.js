9ad4b6bac3df451852c3de84974c3be871fc2c87
PradhumnRaghuwanshi <124507721+PradhumnRaghuwanshi@users.noreply.github.com>
2025-06-21 17:54:46 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { componentTagger } from "lovable-tagger";
 
-
-// https://vite.dev/config/
-export default defineConfig({
-    base: '/', // 👈 This ensures correct path resolution on Render
-  plugins: [react(), tailwindcss()],
-})
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => ({
+  
+  plugins: [
+    react(),
+    mode === 'development' &&
+    componentTagger(),
+  ].filter(Boolean),
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },build: {
+    outDir: 'dist',
+  },
+}));
